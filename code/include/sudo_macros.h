605d3751b9955fc9eec1ff2b17d6ad5757f1dfc4
hark130 <hark130@yahoo.com>
2025-06-21 16:35:31 -0500

    dev: Added a common macro
    SUDO_BOARD_LEN
    
    Changes to be committed:
    	modified:   code/include/sudo_macros.h
    
    Changes not staged for commit:
    	modified:   code/src/sum_dock.c
    
    Untracked files:
    	code/include/sudo_board.h
    	code/include/sudo_memory.h
    	code/include/sudo_validation.h
    	code/src/sudo_board.c
    	code/src/sudo_memory.c
    	code/src/sudo_validation.c

--- code/include/sudo_macros.h
+++ code/include/sudo_macros.h
@@ -15,6 +15,8 @@
     #define NULL ((void *)0)  // Just in case it's not already defined
 #endif  /* NULL */
 // SUDO_MAX_SZ
-#define SUDO_MAX_SZ (~(size_t)0)    // Library's value for the maximum size_t value
+#define SUDO_MAX_SZ (~(size_t)0)  // Library's value for the maximum size_t value
+// SUDO_BOARD_LEN
+#define SUDO_BOARD_LEN ((size_t)81)  // Expected length of the board string and size of game board
 
 #endif  /* __SUDO_MACROS__ */
