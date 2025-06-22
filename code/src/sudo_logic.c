2140eef5d0b609adbb8cbb481390d4381d675975
hark130 <hark130@yahoo.com>
2025-06-21 20:35:11 -0500

    dev: Interim commit of SUDO logic
    Most of the DEBUGGING is commented out.
    The code compiles.
    It mostly works.
    However, there's a flaw in the logic...
    
    Once it finds a strategy 1 match, it stops looking.
    I need to refactor the logic to keep looking.
    If a 2nd match is found, return ENODATA.
    Otherwise, it found the real answer.
    
    Changes to be committed:
    	modified:   code/src/sudo_logic.c

--- code/src/sudo_logic.c
+++ code/src/sudo_logic.c
@@ -39,7 +39,25 @@ char check_for_match(char board[9][9], int row, int col, int *errnum);
 
 /*
  *  Description:
- *      Fill the miss_row array with all the characters missing from board[row]*.
+ *      Determine the grid containing the intersection of row and col.
+ *      1 2 3
+ *      4 5 6
+ *      7 8 9
+ *
+ *  Args:
+ *      row: The row index.
+ *      col: The column index.
+ *      errnum: [Out] Provide feedback on execution.
+ *
+ *  Returns:
+ *      A grid number 1-9 on success (errnum is set to ENOERR), 0 on error (errnum is set
+ *      accordingly).
+ */
+int determine_grid(int row, int col, int *errnum);
+
+/*
+ *  Description:
+ *      Fill the miss_col array with all the characters missing from board*[col].
  *
  *  Args:
  *      board: A two-dimenstional array.
@@ -53,7 +71,8 @@ int fill_missing_col(char board[9][9], char miss_col[9], int col);
 
 /*
  *  Description:
- *      Fill the miss_row array with all the characters missing from board[row]*.
+ *      Fill the miss_grid array with all the characters missing from the 3x3 grid containing
+ *      board[row][col].
  *
  *  Args:
  *      board: A two-dimenstional array.
@@ -172,6 +191,8 @@ bool is_game_over(char board[81])
         results = is_game_really_over(game);
     }
 
+    // TO DO: DON'T DO NOW... verify each column, row, and grid only has one of each character
+
     // DONE
     return results;
 }
@@ -213,7 +234,7 @@ char check_for_match(char board[9][9], int row, int col, int *errnum)
     char miss_row[9] = { '\0' };  // Characters missing from the row
     char miss_col[9] = { '\0' };  // Characters missing from the col
     char miss_grd[9] = { '\0' };  // Characters missing from the grid
-    // char overlap = '\0';          // Overlap character between all three arrays
+    char overlap = '\0';          // Overlap character between all three arrays
 
     // INPUT VALIDATION
     if (NULL == board)
@@ -225,25 +246,72 @@ char check_for_match(char board[9][9], int row, int col, int *errnum)
     // Get missing row characters
     if (ENOERR == results)
     {
+        // FPRINTF_ERR("MISSING ROW CONTENTS BEFORE: %s\n", miss_row);  // DEBUGGING
         results = fill_missing_row(board, miss_row, row);
-        FPRINTF_ERR("MISSING ROW CONTENTS: %s\n", miss_row);  // DEBUGGING
+        // FPRINTF_ERR("MISSING ROW CONTENTS AFTER:  %s\n", miss_row);  // DEBUGGING
     }
     // Get missing col characters
     if (ENOERR == results)
     {
+        // FPRINTF_ERR("MISSING COL %d CONTENTS BEFORE: %s\n", col, miss_col);  // DEBUGGING
         results = fill_missing_col(board, miss_col, col);
+        // FPRINTF_ERR("MISSING COL %d CONTENTS AFTER:  %s\n", col, miss_col);  // DEBUGGING
     }
     // Get missing grid characters
     if (ENOERR == results)
     {
+        // FPRINTF_ERR("MISSING ROW %d COL %d CONTENTS BEFORE: %s\n", row, col, miss_grd);  // DEBUGGING
         results = fill_missing_grid(board, miss_grd, row, col);
+        // FPRINTF_ERR("MISSING ROW %d COL %d CONTENTS AFTER:  %s\n", row, col, miss_grd);  // DEBUGGING
     }
     // Find overlap
     if (ENOERR == results)
     {
-        // TO DO: DON'T DO NOW... DEFINE THIS FUNCTIONALITY
-        results = ENOSYS;  // DEBUGGING
-        PRINT_ERRNO(results);  // DEBUGGING
+        results = ENODATA;  // Default condition
+        for (int row_index = 0; row_index < 9; row_index++)
+        {
+            if ('\0' == miss_row[row_index])
+            {
+                break;  // End of line
+            }
+            for (int col_index = 0; col_index < 9; col_index++)
+            {
+                if ('\0' == miss_col[col_index])
+                {
+                    break;  // End of line
+                }
+                else if (miss_row[row_index] != miss_col[col_index])
+                {
+                    continue;  // Not a match... keep looking
+                }
+                for (int grid_index = 0; grid_index < 9; grid_index++)
+                {
+                    if ('\0' == miss_grd[grid_index])
+                    {
+                        break;  // End of line
+                    }
+                    else if (miss_row[col_index] != miss_grd[grid_index])
+                    {
+                        continue;  // Not a match... keep looking
+                    }
+                    else
+                    {
+                        FPRINTF_ERR("FOUND A MATCH BETWEEN %c AND %c AND %c!\n", miss_row[row_index], miss_col[col_index], miss_grd[grid_index]);  // DEBUGGING
+                        overlap = miss_grd[grid_index];  // Found it!
+                        results = ENOERR;  // Success
+                        break;
+                    }
+                }
+                if (ENOERR == results)
+                {
+                    break;  // Found it!
+                }
+            }
+            if (ENOERR == results)
+            {
+                break;  // Found it!
+            }
+        }
     }
 
     // DONE
@@ -251,19 +319,90 @@ char check_for_match(char board[9][9], int row, int col, int *errnum)
     {
         *errnum = results;
     }
-    return results;
+    return overlap;
+}
+
+
+int determine_grid(int row, int col, int *errnum)
+{
+    // LOCAL VARIABLES
+    int results = ENOERR;  // Results of execution
+    int grid_num = 0;      // 1-9 on success
+
+    // INPUT VALIDATION
+    results = validate_row_and_col(row, col);
+
+    // DETERMINE IT
+    if (ENOERR == results)
+    {
+        switch (row)
+        {
+            case 0:
+            case 1:
+            case 2:
+                grid_num = (col + 3) / 3;
+                break;
+            case 3:
+            case 4:
+            case 5:
+                grid_num = ((col + 3) / 3) + 3;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                grid_num = ((col + 3) / 3) + 6;
+                break;
+            default:
+                results = EINVAL;  // How did we get here?!
+        }
+    }
+
+    // DONE
+    if (NULL != errnum)
+    {
+        *errnum = results;
+    }
+    return grid_num;
 }
 
 
 int fill_missing_col(char board[9][9], char miss_col[9], int col)
 {
     // LOCAL VARIABLES
-    int results = ENOERR;  // Results of execution
+    int results = ENOERR;           // Results of execution
+    int found[10] = { 0 };          // Keep track of chars found by index (index 1 represents '1')
+    char *tmp_miss_col = miss_col;  // Iterating pointer into miss_col
 
     // INPUT VALIDATION
     if (NULL == board || NULL == miss_col)
     {
         results = EINVAL;  // We shall not abide NULL pointers
+    }
+
+    // SETUP
+    if (ENOERR == results)
+    {
+        memset(miss_col, '\0', 9 * sizeof(char));
+    }
+
+    // FILL IT
+    if (ENOERR == results)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            if (SUDO_EMPTY_GRID != board[i][col])
+            {
+                found[board[i][col] - '0'] = 1;  // Found one
+            }
+        }
+        for (int i = 1; i <= 9; i++)
+        {
+            if (0 == found[i])
+            {
+                *tmp_miss_col = i + '0';
+                tmp_miss_col++;
+            }
+        }
     }
 
     // DONE
@@ -274,12 +413,49 @@ int fill_missing_col(char board[9][9], char miss_col[9], int col)
 int fill_missing_grid(char board[9][9], char miss_grid[9], int row, int col)
 {
     // LOCAL VARIABLES
-    int results = ENOERR;  // Results of execution
+    int results = ENOERR;             // Results of execution
+    int found[10] = { 0 };            // Keep track of chars found by index (index 1 represents '1')
+    char *tmp_miss_grid = miss_grid;  // Iterating pointer into miss_col
+    int grid_num = 0;                 // Grid number containing row and col
+    int start_row = 0;                // Iterate over the grid's rows
+    int start_col = 0;                // Iterate over the grid's cols
 
     // INPUT VALIDATION
     if (NULL == board || NULL == miss_grid)
     {
         results = EINVAL;  // We shall not abide NULL pointers
+    }
+
+    // FILL IT
+    // Determine which grid row and col is in
+    if (ENOERR == results)
+    {
+        grid_num = determine_grid(row, col, &results);
+        // FPRINTF_ERR("ROW %d AND COL %d IS IN GRID %d\n", row, col, grid_num);  // DEBUGGING
+    }
+    // Get the values from that grid
+    if (ENOERR == results)
+    {
+        start_row = 3 * ((grid_num - 1) / 3);
+        start_col = 3 * ((grid_num - 1) % 3);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (SUDO_EMPTY_GRID != board[start_row + i][start_col + j])
+                {
+                    found[board[start_row + i][start_col + j] - '0'] = 1;  // Found one
+                }
+            }
+        }
+        for (int i = 1; i <= 9; i++)
+        {
+            if (0 == found[i])
+            {
+                *tmp_miss_grid = i + '0';
+                tmp_miss_grid++;
+            }
+        }
     }
 
     // DONE
@@ -318,9 +494,9 @@ int fill_missing_row(char board[9][9], char miss_row[9], int row)
         }
         for (int i = 1; i <= 9; i++)
         {
-            if (0 != found[i])
+            if (0 == found[i])
             {
-                *tmp_miss_row = i;
+                *tmp_miss_row = i + '0';
                 tmp_miss_row++;
             }
         }
@@ -339,6 +515,16 @@ bool is_empty_intersection(char board[9][9], int row, int col, int *errnum)
 
     // INPUT VALIDATION
     results = validate_row_and_col(row, col);
+
+    // IS IT EMPTY?
+    if (ENOERR == results)
+    {
+        // FPRINTF_ERR("ROW %d COL %d is %c\n", row, col, board[row][col]);  // DEBUGGING
+        if (SUDO_EMPTY_GRID == board[row][col])
+        {
+            is_empty = true;
+        }
+    }
 
     // DONE
     if (NULL != errnum)
@@ -380,8 +566,10 @@ int make_a_match(char board[9][9], int row, int col)
 
     // VALIDATION
     is_empty = is_empty_intersection(board, row, col, &results);
+    // PRINT_ERROR(IS IT EMPTY?);  // DEBUGGING
     if (true == is_empty)
     {
+        // PRINT_ERROR(IT IS EMPTY!);  // DEBUGGING
         match = check_for_match(board, row, col, &results);
         if ('\0' != match && ENOERR == results)
         {
@@ -407,11 +595,12 @@ int solve_strategy_one(char board[81])
     // SOLVE IT
     do
     {
-        for (int row = 0; row < 3; row++)
+        for (int row = 0; row < 9; row++)
         {
-            for (int col = 0; col < 3; col++)
+            for (int col = 0; col < 9; col++)
             {
                 results = make_a_match(game, row, col);
+                // FPRINTF_ERR("make_a_match() returned [%d] %s\n", results, strerror(results));  // DEBUGGING
                 if (ENODATA != results)
                 {
                     break;  // Break out of the for loops
@@ -455,13 +644,13 @@ int validate_row_and_col(int row, int col)
 
     // INPUT VALIDATION
     // row
-    if (row > 2 || row < 0)
+    if (row > 8 || row < 0)
     {
         results = ERANGE;  // Row is out of range
         FPRINTF_ERR("%s The row %d is out of index\n", DEBUG_ERROR_STR, row);
     }
     // col
-    if (col > 2 || col < 0)
+    if (col > 8 || col < 0)
     {
         results = ERANGE;  // Column is out of range
         FPRINTF_ERR("%s The column %d is out of index\n", DEBUG_ERROR_STR, col);
